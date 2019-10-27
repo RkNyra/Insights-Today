97fd252a9828a0b9e155a7a2e893d655c0d43abe
RKNyra <rk.nyra@gmail.com>
2019-10-27 11:35:40 +0300

    create the app's navbar to enable users to navigate to various sections of the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from config import DevConfig
+from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
 
 # Initializing application
 app = Flask(__name__)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+fa = FontAwesome(app)
 
 from .main import views
