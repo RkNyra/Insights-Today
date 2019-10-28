33891a94c0e298ff084999424a530352a59a7ef3
RKNyra <rk.nyra@gmail.com>
2019-10-28 15:20:19 +0300

    update the user profile page to have a photo update feature

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request,redirect,url_for,abort
 from . import main
 from ..models import User
 from .forms import SharePostForm, UpdateProfile
-from ..import db
+from ..import db, photos
 from flask_login import login_required
 
 
@@ -42,7 +42,7 @@ def profile(uname):
 
     return render_template("profile/profile.html", user = user)
 
-# update profile page
+# update profile page - update user bio
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
@@ -60,3 +60,15 @@ def update_profile(uname):
 
         return redirect(url_for('.profile',uname=user.username))
     return render_template('profile/update.html',form =form)
+
+# update prof pic
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
