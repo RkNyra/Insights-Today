22d427748db9c05f4c1a5beae399dcc4060f7455
RKNyra <rk.nyra@gmail.com>
2019-10-28 13:50:19 +0300

    create the user profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, request,redirect,url_for
+from flask import render_template, request,redirect,url_for,abort
 from . import main
+from ..models import User
 from .forms import SharePostForm
 from flask_login import login_required
 
@@ -29,3 +30,12 @@ def sharepost():
     
     title = 'Insights Today'
     return render_template('sharepost.html', title=title, SharePostForm=form)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
